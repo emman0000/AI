50c8a06f21be544da374cea325ba932d1ce3d3f7
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2025-02-10 08:50:18 +0500

    Create Heuristic_code.py

---
+++ Lab 3/Heuristic_code.py
@@ -0,0 +1,33 @@
+from queue import PriorityQueue
+
+graph = {
+    'A': [('B', 5), ('C', 8)],
+    'B': [('D', 10)],
+    'C': [('E', 3)],
+    'D': [('F', 7)],
+    'E': [('F', 2)],
+    'F': []
+}
+
+def best_first(graph, start, goal):
+    visited = set()
+    pq = PriorityQueue()
+    pq.put((0, start))  # Heuristic search is meant for a quicker more efficient method than brute force
+    # we want to find the best possible searching method
+    while not pq.empty():
+        cost, node = pq.get()
+        if node not in visited:
+            print(node, end='')
+            visited.add(node)
+        if node == goal:
+            print("\nGoal Reached!")
+            return True
+        for neighbor, weight in graph[node]:
+            if neighbor not in visited:
+                pq.put((weight, neighbor))
+    print("\nGoal not reachable!")
+    return False
+
+# Example usage: 
+print("Best-First Search Path:")
+best_first(graph, 'A', 'F')
