5816f1afb66e06f22ad0b688d38975d1a5c3108f
Emman Abrar Ali <142867499+emman0000@users.noreply.github.com>
2025-04-17 00:27:55 +0500

    Update task1.py

--- Assignments/Assignment 2/task1.py
+++ Assignments/Assignment 2/task1.py
@@ -1,1 +1,9 @@
-
+#code snippet only
+def find_peak(N):
+  current = 0
+  while current < N:
+    if query(current) < query(current +1):
+      current += 1
+    else:
+      break
+  return current 
